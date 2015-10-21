63dd43e215c8c14083fa4feb063857bb82f5fac4
quindai <randy.quindai@gmail.com>
2015-10-21 11:28:29 -0300

    Versão 0.0.2 finalizada

--- milow/www/js/app.js
+++ milow/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var milow = angular.module('milow', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,10 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+  milow.controller('FillCtrl', function ($scope, $http) {
+    $http.get('data.json').success(function(data) {
+       $scope.ditados = data;
+   });
+});
